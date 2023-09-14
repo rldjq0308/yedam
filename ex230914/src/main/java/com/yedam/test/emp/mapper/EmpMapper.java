769d690efc15dd1685ba60b9c80b13ca379e5e34
rldjq0308 <58042921+rldjq0308@users.noreply.github.com>
2023-09-14 17:49:45 +0900

    2023-09-14 스프링 2일차

---
+++ ex230914/src/main/java/com/yedam/test/emp/mapper/EmpMapper.java
@@ -0,0 +1,25 @@
+package com.yedam.test.emp.mapper;
+
+import java.util.List;
+
+import com.yedam.test.emp.service.EmpVO;
+
+public interface EmpMapper {
+	//전체조회
+	public List<EmpVO> selectEmpAllList(EmpVO empVO);
+	
+	//단건조회
+	public EmpVO selectEmpInfo(EmpVO empVO);
+	
+	//등록
+	public int insertEmpInfo(EmpVO empVO);
+	
+	//수정 - 급여를 정해진 비율로 인상
+	public int updateEmpSal(int employeeId, int raise);
+	
+	//수정 - 사원 정보 수정
+	public int updaetEmpInfo(EmpVO empVO);
+	
+	//삭제
+	public int deleteEmpInfo(int employeeId);
+}
