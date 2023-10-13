ec8334975fe0ef4bfccc5cec3cde4d50e77a84e2
rldjq0308 <58042921+rldjq0308@users.noreply.github.com>
2023-10-13 12:17:48 +0900

    2023-10-13 예담 스프링 프로젝트들

---
+++ spring/src/main/java/com/yedam/app/java/spring/Chef.java
@@ -0,0 +1,10 @@
+package com.yedam.app.java.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Chef {
+	public void cooking() {
+		System.out.println("요리를 시작합니다.");
+	}
+}
